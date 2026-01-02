f0f991bb86f64bebbc5e5182deef1dab7f67e24d
Thuria234naing <113816550+theYE-8266-viren-Nai@users.noreply.github.com.>
2026-01-02 14:21:58 +0630

    the latest update is that the database migrate

---
+++ src/routes/movieRoutes.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+const router = express.Router()
+
+router.get("/" , (req,res)=>{
+    res.json("hello from the router")
+})
+
+export default router
--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 import express from 'express'
+import {config} from 'dotenv'
+import {connectDB , disconnectDB } from './config/db.js'
+//import Routes
+import movieRoutes from './routes/movieRoutes.js'
 
+config()
+connectDB()
 const app = express()
+
+app.get("/hello" , (req,res)=>{
+    res.json({message : "Hello World"})
+})
+
+app.use("/movies" , movieRoutes)
 
 const PORT = 5000;
 const server = app.listen(PORT , () => {
